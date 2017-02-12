d98f170a43eaa0fb6494f5239dfd2b27f244ca9e
Mauricio Miranda <maurimiranda@gmail.com>
2017-02-12 12:06:53 -0300

    Add i18n support

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-$(document).ready(function(){
-  // var map = L.map('map', {
-  // zoomControl: false
-  // }).setView([21.731, -79.569], 7);
-  //
-  // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  //   	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  //   	maxZoom: 18,
-  //   	id: 'elpbatista.n56ifpal',
-  //   	accessToken: 'pk.eyJ1IjoiZWxwYmF0aXN0YSIsImEiOiJ4YlBmNmlJIn0.x0lZKcK907BWTS4LV9dkHA',
-  // }).addTo(map);
-  //
-  // L.control.zoom({
-  //   	position: 'bottomright'
-  // }).addTo(map);
+$(function() {
+
 });
