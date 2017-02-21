441f8e433d17a76f4a31a2c4abe56aa6837603c8
Mauricio Miranda <maurimiranda@gmail.com>
2017-02-21 11:26:14 -0300

    Add about section. Remove animations. Add registration button.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(function() {
+  var count = 6;
+  var left = $(window).width() / (count + 2);
 
+  for (var i = 1; i <= count; i++) {
+    var cloud = $('<div>');
+    cloud.addClass('cloud');
+    cloud.width(getRandom(10, 20) + '%');
+    cloud.height('200px');
+    cloud.offset({ top: 10 * getRandom(1, 30), left: left * i });
+
+    cloud.appendTo($('.sky'));
+  }
 });
+
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
