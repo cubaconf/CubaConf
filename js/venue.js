7911395b034cc223c0b0e2afa0c3cd5bf14580ad
elMor3no <pmdcuba@gmail.com>
2019-03-12 17:48:57 -0300

    updating location
    Cambio de sede a universidad de habana

--- js/venue.js
+++ js/venue.js
@@ -1,10 +1,11 @@
 $(function() {
-  var map = L.map('map').setView([23.13967, -82.35106], 15);
+  var map = L.map('map').setView([23.1368405, -82.3815061], 15);
 
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors</a>',
     maxZoom: 18,
   }).addTo(map);
 
-  L.marker([23.13967, -82.35106]).addTo(map);
+  L.marker([23.1368405, -82.3815061]).addTo(map);
 });
+             
